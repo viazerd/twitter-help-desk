ee7ece19b554292920202e70a6c02242dfe8d7e5
Sandeep Vyas <viazerd@users.noreply.github.com>
2019-01-19 21:15:32 +0530

    Dummt front end and basic landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import{library} from '@fortawesome/fontawesome-svg-core';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faTwitter} from "@fortawesome/free-brands-svg-icons/";
+import logo from './logo.png'
+library.add(faTwitter);
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+
+        return (
+            <Router>
+            <div className="App">
+                <div className="background-image">
+
+                <nav className="navbar sticky-top navbar-expand-lg navbar-primary bg-light">
+                    <a className="navbar-brand" href="https://twitter.com/">
+                        <img src={logo} width="30" height="30" alt="twitter" target="_blank"/>
+                    </a>
+
+                    <Link to="/" className="navbar-brand">Twitter Help Desk</Link>
+
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav mr-auto">
+                        </ul>
+                        <ul className="navbar-nav">
+                            <li className="navbar-item">
+                                <Link to="/" className="nav-link">Help</Link>
+                            </li>
+                            <li>
+                                <Link to="/create" className="nav-link">About</Link>
+                            </li>
+                        </ul>
+                    </div>
+
+                </nav>
+
+
+
+                    <button className=" twitterIcon shadow-lg rounded btn btn-outline-primary " >
+                        <FontAwesomeIcon icon={faTwitter}
+                                         size="7x"
+                        />
+                    </button>
+
+                </div>
+
+            </div>
+    </Router>
+
+
+
+
+        );
+    }
 }
 
 export default App;
