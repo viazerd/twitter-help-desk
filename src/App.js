5f3c0bc745f078e11e54a6f8d18724eb5f8afeb9
Sandeep Vyas <viazerd@users.noreply.github.com>
2019-01-25 19:59:18 +0530

    Minor Changes in frontend
    -- Created Dummy Landing Page
    -- Separated SignUp component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route,Link} from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css"
+import LadingPage from './components/LandingPage/landing-page.component'
 import SignUpPage from './components/SignUp/signUp-page.component';
-import LoginForm from './components/Login/login-form.component'
 import NavBar from './components/Navbar/navbar.component';
 
 
@@ -14,8 +14,9 @@ class App extends Component {
 
           <div className="App container-fluid">
 
-            <NavBar/>
-            <Route path ='/' exact component={SignUpPage}/>
+              <NavBar/>
+              <Route path ='/' exact component={LadingPage}/>
+              <Route path ='/signUp' exact component={SignUpPage}/>
 
           </div>
         </Router>
